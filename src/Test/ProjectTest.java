c6971b04e0e01df009fc795385bd7d134cde0d13
vigorweijia <vigorweijia@gmail.com>
2019-03-14 23:05:23 +0800

    JiaoWuTest

---
+++ src/Test/ProjectTest.java
@@ -0,0 +1,11 @@
+package Test;
+
+import java.util.ArrayList;
+
+public class ProjectTest {
+    public static void main(String args[]) {
+        ArrayList A = new ArrayList();
+        A.add(1);
+        System.out.println();
+    }
+}
