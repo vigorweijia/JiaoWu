19f551136f400fe20501c6e70c5f7a6fff8fce39
vigorweijia <vigorweijia@gmail.com>
2019-03-15 22:27:19 +0800

    修改了更新早晨时间时可能出现的bug

--- src/MyPkg/MorningCourse.java
+++ src/MyPkg/MorningCourse.java
@@ -8,9 +8,9 @@ public class MorningCourse {
     public static final int CourseFirst = 1; //8:00
     public static final int CourseSecond = 2; //9:00
     public static final int CourseThird = 3; //10:00
-    public static int Monday = -1;
-    public static int Tuesday = -1;
-    public static int Wednesday = -1;
-    public static int Thrusday = -1;
-    public static int Friday = -1;
+    public static int Monday = 99;
+    public static int Tuesday = 99;
+    public static int Wednesday = 99;
+    public static int Thrusday = 99;
+    public static int Friday = 99;
 }
